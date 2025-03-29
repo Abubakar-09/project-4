c514c0fbd568a972f64b0b2acdb1418d5938159b
AbuBakar Chohan <132102392+Abubakar-09@users.noreply.github.com>
2025-03-30 03:15:56 +0500

    Add files via upload

---
+++ blog/app/page.js
@@ -0,0 +1,15 @@
+import Bottom_Sec from "./components/Bottom_Sec";
+import Cards from "./components/Cards";
+import HeroSection from "./components/HeroSection";
+import NewsSection from "./components/NewsSection";
+
+export default function Home() {
+  return (
+    <>
+    <HeroSection />
+    <NewsSection />
+    <Cards />
+    <Bottom_Sec />
+    </>
+  );
+}
